c244f51e79979d25cb737b20484ea477145fd9c8
Michael Bull <mikebull94@gmail.com>
2016-11-26 20:09:11 +0000

    Add item preview custom element

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import { FrameworkConfiguration } from 'aurelia-framework';
 export function configure(config: FrameworkConfiguration): void {
     config.globalResources([
         './comment',
+        './item-preview',
         './nav-bar',
         './news-list',
         './paginator'
---
+++ src/components/item-preview.ts
@@ -0,0 +1,6 @@
+import { bindable, customElement } from 'aurelia-framework';
+
+@customElement('hn-item-preview')
+export class ItemPreview {
+    @bindable() item: any;
+}
