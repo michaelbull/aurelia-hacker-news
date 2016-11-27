11c8717444407a61614e8a6fb13ab1741e3e000d
Michael Bull <mikebull94@gmail.com>
2016-11-27 19:15:03 +0000

    Fix failing test spec

--- test/unit/app.spec.ts
+++ test/unit/app.spec.ts
@@ -3,16 +3,10 @@ import { RouteConfig, RouterConfiguration } from 'aurelia-router';
 
 class RouterConfigurationStub extends RouterConfiguration {
     title: string;
-    unknownRoute: string;
     routes: RouteConfig[] = Array<RouteConfig>();
 
     mapRoute(config: RouteConfig): RouterConfiguration {
         this.routes.push(config);
-        return this;
-    }
-
-    mapUnknownRoutes(config: string): RouterConfiguration {
-        this.unknownRoute = config;
         return this;
     }
 }
@@ -27,10 +21,6 @@ describe('App', () => {
 
             testee = new App();
             testee.configureRouter(config, jasmine.createSpyObj('router', ['reset']));
-        });
-
-        it('should map unknown routes to not-found', () => {
-            expect(config.unknownRoute).toBe('./pages/not-found');
         });
 
         it('should set the title to Aurelia HN', () => {
