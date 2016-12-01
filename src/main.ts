1349aa32da6f3b47514b1072df932512f5068a5a
Michael Bull <mikebull94@gmail.com>
2016-12-01 23:51:48 +0000

    Disable development logging in production
    Closes #8

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import '../style/index.scss';
 import { Aurelia } from 'aurelia-framework';
 
 export function configure(aurelia: Aurelia): void {
-    aurelia.use
-        .standardConfiguration()
-        .developmentLogging();
+    aurelia.use.standardConfiguration();
+
+    if (process.env.NODE_ENV !== 'production') {
+        aurelia.use.developmentLogging();
+    }
 
     aurelia.start().then(() => aurelia.setRoot('app'));
 }
--- webpack.config.js
+++ webpack.config.js
@@ -125,7 +125,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: process.env.NODE_ENV
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
             }
         }),
         new AureliaWebPackPlugin(),
