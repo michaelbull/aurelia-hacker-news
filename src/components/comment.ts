912025ed4c4afa82c1d9c7c2474e4fab830d1827
Michael Bull <mikebull94@gmail.com>
2016-12-01 23:24:08 +0000

    Prevent links to items from redirecting to real HN

--- src/components/comment.ts
+++ src/components/comment.ts
@@ -33,4 +33,25 @@ export class Comment {
     toggle(): void {
         this.expanded = !this.expanded;
     }
+
+    text(): string {
+        let raw: string = this.comment.text;
+        let url: RegExp = new RegExp(
+            '<a href="https:&#x2F;&#x2F;news\.ycombinator\.com&#x2F;item\\?id=(\\d+)" rel="nofollow">' +
+            'https:&#x2F;&#x2F;news\.ycombinator\.com&#x2F;item\\?id=\\d+' +
+            '<\/a>',
+            'g');
+
+        let match: RegExpExecArray | null = url.exec(raw);
+
+        if (match === null) {
+            return raw;
+        }
+
+        let replacement: string = `<a
+          href="https://mikebull94.github.io/aurelia-hacker-news/#/item/${match[1]}" 
+          rel="nofollow">#${match[1]}</a>`;
+
+        return raw.replace(url, replacement);
+    }
 }
