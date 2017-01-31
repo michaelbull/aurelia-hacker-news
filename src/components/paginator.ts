1138889990d578ef7c48f73da091d380d2bde9df
Michael Bull <mikebull94@gmail.com>
2017-01-31 18:28:03 +0000

    Add helper methods to paginator

--- src/components/paginator.ts
+++ src/components/paginator.ts
@@ -8,4 +8,12 @@ export class Paginator {
     @bindable() private readonly current: number;
     @bindable() private readonly total: number;
     @bindable() private readonly route: string;
+
+    prev(): number {
+        return this.current > 1 ? this.current - 1 : 1;
+    }
+
+    next(): number {
+        return this.current < this.total ? this.current + 1 : this.total;
+    }
 }
