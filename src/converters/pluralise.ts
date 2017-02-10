9e73ef967fa3b9ddc1347f242d623799c75a010e
Michael Bull <mikebull94@gmail.com>
2017-02-10 23:50:30 +0000

    Use ternary operator in PluraliseValueConverter

--- src/converters/pluralise.ts
+++ src/converters/pluralise.ts
@@ -3,10 +3,6 @@ import { valueConverter } from 'aurelia-framework';
 @valueConverter('pluralise')
 export class PluraliseValueConverter {
     toView(value: number, text: string): string {
-        if (value > 1) {
-            return `${text}s`;
-        } else {
-            return text;
-        }
+        return value > 1 ? `${text}s` : text;
     }
 }
