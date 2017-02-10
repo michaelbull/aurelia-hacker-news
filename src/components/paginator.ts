0278a64a67d0c36dea0fe57571252cfb5a86e306
Michael Bull <mikebull94@gmail.com>
2017-02-10 23:26:59 +0000

    Fix pagination

--- src/components/paginator.ts
+++ src/components/paginator.ts
@@ -8,12 +8,4 @@ export class Paginator {
     @bindable() readonly current: number;
     @bindable() readonly total: number;
     @bindable() readonly route: string;
-
-    prev(): number {
-        return this.current > 1 ? this.current - 1 : 1;
-    }
-
-    next(): number {
-        return this.current < this.total ? this.current + 1 : this.total;
-    }
 }
