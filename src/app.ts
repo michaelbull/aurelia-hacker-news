9cbd56d78ae1415d48c4488bb175ab50625ac190
Michael Bull <mikebull94@gmail.com>
2018-02-27 12:30:29 +0000

    Simplify component templates

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import {
     RouterConfiguration
 } from 'aurelia-router';
 import * as NProgress from 'nprogress';
+import '../assets/aurelia-logo.png';
 import '../style/index.scss';
 import { ScrollToTopStep } from './services/scroll-to-top-step';
 
